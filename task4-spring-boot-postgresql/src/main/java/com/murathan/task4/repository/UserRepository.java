30147eaef32edab3f0e484b552bcf73d01ec705a
anonym <152076274+Anonymbordo@users.noreply.github.com>
2026-07-17 23:27:30 +0300

    Add task 4 user repository

---
+++ task4-spring-boot-postgresql/src/main/java/com/murathan/task4/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.murathan.task4.repository;
+
+import com.murathan.task4.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+}
